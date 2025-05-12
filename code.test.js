96fbc3eb70ea74f647273f7e90eedda4a2d1888c
tesla-a-n <143447224+tesla-a-n@users.noreply.github.com>
2025-05-12 00:07:26 -0600

    simple linear graph and branching graph testing

--- code.test.js
+++ code.test.js
@@ -3,7 +3,7 @@ const jsc = require('jsverify');
 
 eval(fs.readFileSync('code.js')+'');
 
-const testReturnBug =
+const testDFSLinearGraph =
     jsc.forall("bool", function() {
         const graph = {
             'A': ['B'],
@@ -14,4 +14,49 @@ const testReturnBug =
         return JSON.stringify(path) === JSON.stringify(['A', 'B', 'C']);
     });
 
-jsc.assert(testReturnBug);
+const testBFSLinearGraph =
+    jsc.forall("bool", function() {
+        const graph = {
+            'A': ['B'],
+            'B': ['C'],
+            'C': []
+        };
+        const path = breadthFirstSearch(graph, 'A', 'C');
+        return JSON.stringify(path) === JSON.stringify(['A', 'B', 'C']);
+    });
+
+const testDFSBranchingGraph = jsc.forall("bool". function() {
+    const graph = {
+        'A': ['B'. 'C'],
+        'B': ['D', 'E'],
+        'C': ['F'],
+        'D': [],
+        'E': ['G'],
+        'F': [],
+        'G': []
+    };
+    const path = depthFirstSearch(graph, 'A', 'G');
+    return JSON.stringify(path) === JSON.stringify(['A', 'B', 'E', 'G']);
+});
+
+const testBFSBranchingGraph = jsc.forall("bool". function() {
+    const graph = {
+        'A': ['B'. 'C'],
+        'B': ['D', 'E'],
+        'C': ['F'],
+        'D': [],
+        'E': ['G'],
+        'F': [],
+        'G': []
+    };
+    const path = breadthFirstSearch(graph, 'A', 'G');
+    return JSON.stringify(path) === JSON.stringify(['A', 'B', 'E', 'G']);
+});
+
+jsc.assert(testDFSReturnBug);
+jsc.assert(testBFSReturnBug);
+jsc.assert(testBFSBranchingGraph);
+jsc.assert(testDFSBranchingGraph);
+
+
+
